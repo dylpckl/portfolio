62966c18d9ddd9afd5c2e859b35e9a1c885a5662
dylpckl <dylanjbsmith@gmail.com>
2023-03-10 13:40:48 -0500

    About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Project from "./(components)/Project";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,20 +34,52 @@ const projects = [
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-zinc-800 text-zinc-200 p-6 max-w-4xl m-auto min-h-screen">
+    <main className="flex flex-col gap-4 items-center bg-zinc-800 text-zinc-200 max-w-4xl m-auto min-h-screen">
       {/* header */}
-      <header className="">header</header>
+      <header className="bg-sky-600 h-16 w-full flex items-center justify-between text-black">
+        <div className="">ds</div>
 
-      {/* about */}
-      <div className="">about</div>
+        <div className="flex gap-2">
+          <ul className="flex gap-2">
+            <li>
+              <a href="#about">about</a>
+            </li>
+            <li>
+              <a href="#work">work</a>
+            </li>
+            <li>
+              <a href="#contact">contact</a>
+            </li>
+          </ul>
+          <Link href="/resume">resume</Link>
+        </div>
+      </header>
 
-      {projects.map((project, index) => (
-        <Project
-          key={project.project.title}
-          direction={index % 2 ? "right" : "left"}
-          {...project}
-        />
-      ))}
+      {/* intro */}
+      <section className="bg-pink-600 w-full min-h-96 h-96 flex flex-col justify-center items-start pt-7 text-black">
+        <h1 className="text-7xl">Dylan Smith</h1>
+        <h2 className="text-5xl">passionate about all things web related</h2>
+        <p className="mt-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button>click a button</button>
+      </section>
+
+      <section className="w-full">
+        {projects.map((project, index) => (
+          <Project
+            key={project.project.title}
+            direction={index % 2 ? "right" : "left"}
+            {...project}
+          />
+        ))}
+      </section>
     </main>
   );
 }
